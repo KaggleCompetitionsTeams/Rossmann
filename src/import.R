bc9f5312c35eed0956be94634ef505c023e634af
Marc-Antoine Giuliani <marcantoinegiuliani@gmail.com>
2015-11-18 16:53:17 +0100

    Fichier import.R

---
+++ src/import.R
@@ -0,0 +1,20 @@
+# Marc-Antoine Giuliani
+
+###############################################################################
+# Import libraries#############################################################
+###############################################################################
+
+libs <- c('readr')
+sapply(libs, require, character.only=TRUE)
+
+###############################################################################
+# Import data##################################################################
+###############################################################################
+
+train_path <- "~/Documents/kaggle_data/kaggle_rossmann/train.csv"
+test_path <- "~/Documents/kaggle_data/kaggle_rossmann/test.csv"
+store_path <- "~/Documents/kaggle_data/kaggle_rossmann/store.csv"
+
+train_data <- read_csv(train_path)
+test_data <- read_csv(test_path)
+stores_data <- read_csv(store_path)
