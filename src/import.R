f61277d9720a27fe30fd2681fc6b2994ce1b1a38
Marc-Antoine Giuliani <marcantoinegiuliani@gmail.com>
2015-11-20 10:23:03 +0100

    modified path

--- src/import.R
+++ src/import.R
@@ -9,9 +9,9 @@ sapply(libs, require, character.only=TRUE)
 # Import data##################################################################
 ###############################################################################
 
-train_path <- "~/Documents/kaggle_data/kaggle_rossmann/train.csv"
-test_path <- "~/Documents/kaggle_data/kaggle_rossmann/test.csv"
-store_path <- "~/Documents/kaggle_data/kaggle_rossmann/store.csv"
+train_path <- "~/Documents/kaggle_data/kaggle_rossmann/data/train.csv"
+test_path <- "~/Documents/kaggle_data/kaggle_rossmann/data/test.csv"
+store_path <- "~/Documents/kaggle_data/kaggle_rossmann/data/store.csv"
 
 train_data <- read_csv(train_path)
 test_data <- read_csv(test_path)
