4ba4596ad1a5887194ccad66bc450f739e45d528
Marc-Antoine Giuliani <marcantoinegiuliani@gmail.com>
2015-11-24 12:05:40 +0100

    modify import for state holidays

--- src/import.R
+++ src/import.R
@@ -13,6 +13,15 @@ train_path <- "~/Documents/kaggle_data/kaggle_rossmann/data/train.csv"
 test_path <- "~/Documents/kaggle_data/kaggle_rossmann/data/test.csv"
 store_path <- "~/Documents/kaggle_data/kaggle_rossmann/data/store.csv"
 
-train_data <- read_csv(train_path)
-test_data <- read_csv(test_path)
-stores_data <- read_csv(store_path)
+cat("reading the train and test data\n")
+train <- read_csv(train_path,
+                  col_types = list(
+                    StateHoliday = col_factor(c("0", "a", "b", "c"))
+                  ))
+
+test <- read_csv(test_path,
+                 col_types = list(
+                   StateHoliday = col_factor(c("0", "a", "b", "c"))
+                 ))
+
+stores <- read_csv(store_path)
